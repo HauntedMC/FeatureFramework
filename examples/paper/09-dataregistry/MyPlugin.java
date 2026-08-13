ee5a31befe40eb3afe3f275ee45dcf98ced738e9
Remy Duijsens <remyduijsens@gmail.com>
2026-08-13 14:57:29 +0200

    docs: wire Paper DataRegistry bootstrap

--- examples/paper/09-dataregistry/MyPlugin.java
+++ examples/paper/09-dataregistry/MyPlugin.java
@@ -1,6 +1,19 @@
 package com.example.paperdata;
 
+import nl.hauntedmc.featureframework.paper.host.PaperFeatureHostComposition;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MyPlugin extends JavaPlugin {
+    private PaperFeatureHostComposition<?, ?, ?, ?> featureHost;
+
+    @Override
+    public void onEnable() {
+        featureHost = DataExampleHost.create(this);
+        featureHost.start();
+    }
+
+    @Override
+    public void onDisable() {
+        if (featureHost != null) featureHost.stop();
+    }
 }
