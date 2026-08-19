64a83350ec0319911db05eceee8508386ba0130f
Remy Duijsens <remyduijsens@gmail.com>
2026-08-19 09:23:28 +0200

    Fix token service lint

--- featureframework-toolkit/src/main/java/nl/hauntedmc/featureframework/toolkit/token/TokenService.java
+++ featureframework-toolkit/src/main/java/nl/hauntedmc/featureframework/toolkit/token/TokenService.java
@@ -4,7 +4,6 @@ import java.security.SecureRandom;
 import java.time.Duration;
 import java.util.Base64;
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
